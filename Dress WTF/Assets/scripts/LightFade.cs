5bb253d56353d066807014c00f901ed3144fcf21
john oh <johno@unity3d.com>
2015-03-11 08:11:02 +0900

    light fade

---
+++ Dress WTF/Assets/scripts/LightFade.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFade : MonoBehaviour {
+
+	public float fadeSpeed = 1;
+	float initIntensity = 1;
+	Light lit;
+
+	float fadeMode = 0; 	// 0 : no  1 : incr  -1 : decr
+
+	// Use this for initialization
+	void Awake () {
+		lit = gameObject. GetComponent<Light>();
+		initIntensity = lit.intensity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( fadeMode != 0 ) {
+			lit.intensity += fadeMode * fadeSpeed * Time.deltaTime;
+			if( lit.intensity < 0 ) {
+				lit.intensity = 0;
+				fadeMode = 0;
+			}
+			if( lit.intensity > initIntensity ) {
+				lit.intensity = initIntensity;
+				fadeMode = 0;
+			}
+		}
+	}
+
+	public void ToggleFade() {
+		if( fadeMode >= 0 && lit.intensity > 0) {
+			fadeMode = -1;
+		} else {
+			fadeMode = 1;
+		}
+	}
+
+}
