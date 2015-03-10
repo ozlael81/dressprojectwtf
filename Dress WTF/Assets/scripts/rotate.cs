e83122b887087caa0d90e60f130d13babff05256
john oh <johno@unity3d.com>
2015-03-11 07:37:51 +0900

    rotate script

---
+++ Dress WTF/Assets/scripts/rotate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+	public float speed = 20.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( Input.GetMouseButton(0))
+			transform.Rotate(Vector3.up * speed * Time.deltaTime * Input.GetAxis("Mouse X"));
+	}
+}
