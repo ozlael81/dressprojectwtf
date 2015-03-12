8ef06e97341477d5e0c44733b0a5ee00c9c65a88
john oh <johno@unity3d.com>
2015-03-12 09:18:33 +0900

    spawn animation - 설명 참고
    오브젝트에 달아놓으면 오브젝트 생성 시 스케일 에니메이션 함. 명시적으로 MotionStart() 호출해도 됨.

---
+++ Dress WTF/Assets/scripts/ObjectSpawnAnimation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectSpawnAnimation : MonoBehaviour {
+
+	private SmoothSpeedFactor smoothSpeed;
+	public float speed = 1.0f;
+	public float speedResistanceRatio = 1.5f;
+	
+	// Use this for initialization
+	void Start () {
+		smoothSpeed = new SmoothSpeedFactor( speed, speedResistanceRatio);
+		MotionStart();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UpdateMove();
+	}
+	
+	public void MotionStart() {
+		smoothSpeed.StartToMove( 1);
+	}
+	
+	void UpdateMove() {
+		smoothSpeed.updateSpeed();
+		Vector3 scale = new Vector3( 1.0f - smoothSpeed.GetCurrentSpeed(), 1.0f - smoothSpeed.GetCurrentSpeed(), 1.0f - smoothSpeed.GetCurrentSpeed()); 
+		transform.localScale = scale;
+	}
+}
